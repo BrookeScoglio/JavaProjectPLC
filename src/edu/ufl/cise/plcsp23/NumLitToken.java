300d251f1f136f430122220cf43ee60c2894ef0d
hannahohardy <80282921+hannahohardy@users.noreply.github.com>
2023-02-12 23:16:18 -0500

    Update NumLitToken.java

--- src/edu/ufl/cise/plcsp23/NumLitToken.java
+++ src/edu/ufl/cise/plcsp23/NumLitToken.java
@@ -1,17 +1,15 @@
 package edu.ufl.cise.plcsp23;
 
+public class NumLitToken extends Token implements  INumLitToken{
 
-public class NumLitToken extends Token implements INumLitToken {
 
-    public NumLitToken(IToken.Kind kind, int pos, int length, int line, int col, char[] input){
-        super(kind, pos, length, line, col, input);
+    public NumLitToken(IToken.Kind kind, int pos, int length, int line, int column, char[] input) {
+        super(kind, pos, length, line, column, input);
     }
 
-    //returns integer from num literal
     @Override
-    public int getValue() {
-        String number = new String(source, pos, length);
-        return Integer.parseInt(number);
+        public int getValue() {
+            String number = new String(source, pos, length);
+            return Integer.parseInt(number);
+        }
     }
-    //EOF NumLitToken.Java
-}
